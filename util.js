2f7ad17ff6590cd196b3885915989d26a0f01b82
bababoy <68753989+bababoyisntapopularname@users.noreply.github.com>
2021-11-17 19:42:38 +0300

    update

--- util.js
+++ util.js
@@ -1,6 +1,5 @@
 // @author @Bababoy#1524
 // Licensed as idk
-
 function addCss(cssCode) {
   var styleElement = document.createElement("style");
   if (styleElement.styleSheet) {
@@ -11,15 +10,24 @@ function addCss(cssCode) {
   document.getElementsByTagName("head")[0].appendChild(styleElement);
 }
 
+/**
+ * @param {string} url
+ */
 async function $import(url) {
   let css = await fetch(url).then((x) => x.text());
   addCss(css);
 }
+
+/**
+ * @param {string} url
+ */
 async function $require(url) {
   let js = await fetch(url).then((x) => x.text());
   Function(js)();
 }
-window.uBababot = {
-  loadJS: $import,
-  loadCSS: $require,
+
+let uBababot = {
+  cImport: $import,
+  jRequire: $require,
 };
+window["uBababot"] = uBababot;
