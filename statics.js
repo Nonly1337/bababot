c88b6167f7475b4d2fbf81f1a67a3b9454e02f68
bababoy <68753989+bababoyisntapopularname@users.noreply.github.com>
2021-11-20 17:46:48 +0300

    Create statics.js

---
+++ statics.js
@@ -0,0 +1,49 @@
+/**
+ * @description Statics of Bababot
+ */
+const BABABOT_CONSOLE_CSS = `background: #03CEA4;
+color: #FFFFFF;
+font-family: Sans-serif;
+font-size: 2rem;
+display: inline-block;
+border: 5px #A3A5C3 solid;
+padding: 5px;
+margin: 5px;`,
+  BABABOT_CANVAS_CSS = {
+    textAlign: "center",
+    border: "black 1px dotted",
+    paddingLeft: "0",
+    paddingRight: "0",
+    marginLeft: "auto",
+    marginRight: "auto",
+    display: "block",
+  },BABABOT_MENU_ELEMENT_CSS = {
+    border: "2px rgb(8,8,8) solid",
+    background:
+      "linear-gradient(90deg, rgba(30,30,30,1) 0%, rgba(34,34,34,1) 100%)",
+    "border-radius": "0px",
+    color: "#d9d2d2",
+    padding: "5px",
+    "font-family": "monospace",
+    margin: "10px",
+  },BABABOT_UI_MENU_CSS = {
+    display: "none",
+    position: "absolute",
+    "font-family": "Monospace",
+    color: "#d9d2d2",
+    background: "rgb(27,27,27)",
+    border: "5px rgb(188,26,26) solid",
+    transform: "translate(-50%, -50%)",
+    top: "50%",
+    left: "50%",
+    padding: "20px",
+  },BABABOT_TEXT_CSS = {
+    display: "block",
+    position: "absolute",
+    width: "auto",
+    bottom: "11px",
+    right: "250px",
+    color: "#ffffff",
+    "text-shadow": "1px 1px 1px #000000",
+    "font-size": "0.9em",
+  };
