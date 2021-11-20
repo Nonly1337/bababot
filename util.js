2f383eea76785a4e731c7dddf001f0ec306a931f
bababoy <68753989+bababoyisntapopularname@users.noreply.github.com>
2021-11-20 16:49:54 +0300

    Update util.js

--- util.js
+++ util.js
@@ -31,3 +31,14 @@ let uBababot = {
   jRequire: $require,
 };
 window["uBababot"] = uBababot;
+
+Function.prototype.clone = function() {
+    var that = this;
+    var temp = function temporary() { return that.apply(this, arguments); };
+    for(var key in this) {
+        if (this.hasOwnProperty(key)) {
+            temp[key] = this[key];
+        }
+    }
+    return temp;
+};
